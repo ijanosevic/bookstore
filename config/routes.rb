5d34398ae5c54a6e69c52e824d5120e0f23e85f1
Igor Janosevic <igorjanosevic@Igors-MacBook-Pro-2.local>
2022-01-06 13:27:00 +0100

    added basic user and session handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#welcome'
+
+  resources :users
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
+
+# Rails.application.routes.draw do
+#   get 'sessions/welcome'
+#   get 'sessions/new'
+#   get 'sessions/create'
+#   get 'sessions/destroy'
+#   get 'users/new'
+#   get 'users/create'
+#   get 'users/show'
+#   get 'users/edit'
+#   get 'users/update'
+#   get 'users/destroy'
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
